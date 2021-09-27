c3194ad8b65e50841aa64b628bc86bf252185aff
luisluft <luberluft@gmail.com>
2021-09-27 08:14:54 -0400

    add hero component

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,27 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext();
+  const { closeSubmenu } = useGlobalContext();
 
-  return <h2>hero component</h2>;
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            aliquid maxime neque voluptates ipsum rem dolor voluptatem cum
+            laborum vel ipsa qui ea quia, repudiandae facere unde eos quibusdam
+            possimus.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
